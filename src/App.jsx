5815f613d6aedbf9ecdc44d53aa07401014cf288
Ericlei de lima <ericleilima@gmail.com>
2024-12-02 16:11:24 -0300

    Primeira pagina do projeto

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+
+<picture>
+    <img className="arac" src="src/assets/imagens/icons/logo goro 3.png" alt="aranha viuva negra" />
+</picture>
+
+<aside>
+
+<div className="painel">
+ <a href="galeria.html">GALERIA</a>
+     <p>Alguns dos trabalhos realizados.</p>
+</div>
+<div className="painel">
+ <a href="estilosTattoo.html">ESTILOS DE TATTOO</a>
+     <p>Os estilos mais populares.</p>
+</div>
+<div className="painel">
+ <a href="modelo.html">MODELO</a>
+     <p>Seja modelo para um de nossos projetos.</p>
+</div>
+<div className="painel">
+ <a href="relampago.html">RELÂMPAGO</a>
+     <p>Oferta valida por pouco tempo.</p>
+</div>
+<div className="painel">
+ <a href="construcao.html">LOCALIZAÇÃO, REGRAS, ETC</a>
+     <p> Desenhos pequenos.</p>
+</div>
+</aside>
+</main>
+    </>
+  )
+}
+
+export default App
