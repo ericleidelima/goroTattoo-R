337a5a3c69b1196af90fa9e0f5994f61e1ae3c28
Ericlei de lima <ericleilima@gmail.com>
2024-12-04 15:34:44 -0300

    Excluimos icones

--- src/header.jsx
+++ src/header.jsx
@@ -1,6 +1,6 @@
-import LogoI from "/public/icons/logoInstag.png";
-import LogoF from "/public/icons/logoFace.png";
-import LogoW from "/public/icons/logoWhats.png";
+//import LogoI from "/public/icons/logoInstag.png";
+//import LogoF from "/public/icons/logoFace.png";
+//import LogoW from "/public/icons/logoWhats.png";
 
 const Header = () => {
   return (
@@ -12,17 +12,17 @@ const Header = () => {
       </nav>
       <div className="logos">
         <a href="https://www.instagram.com/goro_tattooo/" target="_blank">
-          <img src={LogoI} alt="Instagram" title="Instagram" />
+          <img  alt="Instagram" title="Instagram" />
         </a>
 
         <a
           href="https://www.facebook.com/ericlei.lima.9/photos"
           target="_blank"
         >
-          <img src={LogoF} alt="" title="Facebook" />
+          <img  alt="" title="Facebook" />
         </a>
         <a href="https://wa.me/5541996607553" target="_blank">
-          <img src={LogoW} alt="" title="WhatsApp" />
+          <img  alt="" title="WhatsApp" />
         </a>
       </div>
     </header>
