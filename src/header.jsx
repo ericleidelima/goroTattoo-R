2c59167899b7d946451eb6fd0474162c552ed29b
USER 1 <user@USERs-Mac-mini.local>
2024-12-21 19:37:06 -0300

    Alteração no icone do face

--- src/header.jsx
+++ src/header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
           target="_blank">
-          <img src="src/assets/logoFace.png" alt="" title="Facebook" />
+          <img src="src/assets/logoFace.png" alt="iconeFace" title="Facebook" />
         </a>
         <a href="https://wa.me/5541996607553" target="_blank">
           <img src="public/icons/logoWhats.png" alt="" title="WhatsApp" />
