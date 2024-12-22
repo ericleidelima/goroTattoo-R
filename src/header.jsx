88bc22fe66787dc6714e15dbe1f296ca085d01c8
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 15:56:47 -0300

    Alteração no caminho do icone instag

--- src/header.jsx
+++ src/header.jsx
@@ -1,4 +1,4 @@
-//import LogoI from "/public/icons/logoInstag.png";
+import LogoI from "/public/Icons/LogoInstag.png";
 //import LogoF from "/public/icons/logoFace.png";
 //import LogoW from "/public/icons/logoWhats.png";
 
@@ -8,11 +8,11 @@ const Header = () => {
       <nav>
         <a href="/">Home</a>
         <a href="disponiveis.html">Disponiveis</a>
-        <a href="/src/Pages/Curiosidades">Curiosidades</a>
+        <a href="/src/Pages/Curiosidades.html">Curiosidades</a>
       </nav>
       <div className="logos">
         <a href="https://www.instagram.com/goro_tattooo/" target="_blank">
-          <img src="/public/Imagens/LogoInstag.jpg" alt="Instagram" title="Instagram" />
+          <img src={LogoI} alt="Instagram" title="Instagram" />
         </a>
 
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
