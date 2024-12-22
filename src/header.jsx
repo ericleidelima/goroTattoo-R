b562eb77b8d7a3d966577dc232ef5255a504ae83
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 16:06:08 -0300

    Alteramos o formato do icine face

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,5 @@
 import LogoI from "/public/Icons/LogoInstag.jpg";
-import LogoF from "/public/Icons/logoFace.png";
+import LogoF from "/public/Icons/logoFace.ico";
 //import LogoW from "/public/icons/logoWhats.png";
 
 const Header = () => {
