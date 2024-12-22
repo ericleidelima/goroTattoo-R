508a09767cbe42213f7fc642e7e956549449d64c
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 14:22:09 -0300

    Alrteração no caminho da imagem

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import Footer from "../Footer";
 import Header from "../header";
 
-import logoGoro from "/public/imagens/logo-goro-1.jpg";
+import logoGoro from "./imagens/logo-goro-1.jpg";
 
 const Home = () => {
   return (
