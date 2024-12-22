40c831a85eca7d8d490914bc844119f65c98af4d
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 16:10:21 -0300

    Alteração no caminho do icone face

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,5 @@
 import LogoI from "/public/Icons/LogoInstag.jpg";
-import LogoF from "/./public/Icons/logoFace.ico";
+import LogoF from "/public/Icons/logoFace.jpg";
 //import LogoW from "/public/icons/logoWhats.png";
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
       </nav>
       <div className="logos">
         <a href="https://www.instagram.com/goro_tattooo/" target="_blank">
-          <img src={LogoI} alt="" title="" />
+          {<img src={LogoI} alt="" title="" />}
         </a>
 
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
