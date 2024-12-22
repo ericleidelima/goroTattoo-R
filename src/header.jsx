cedab15f12835ba63687b81e5d2d0c19cd350680
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 16:01:40 -0300

    iserimos o icone do face

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,5 @@
 import LogoI from "/public/Icons/LogoInstag.jpg";
-//import LogoF from "/public/icons/logoFace.png";
+import LogoF from "/public/Icons/logoFace.png";
 //import LogoW from "/public/icons/logoWhats.png";
 
 const Header = () => {
@@ -12,12 +12,12 @@ const Header = () => {
       </nav>
       <div className="logos">
         <a href="https://www.instagram.com/goro_tattooo/" target="_blank">
-          <img src={LogoI} alt="Instagram" title="Instagram" />
+          <img src={LogoI} alt="" title="" />
         </a>
 
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
           target="_blank">
-          <img src="" alt="iconeFace" title="Facebook" />
+          <img src={LogoF} alt="iconeFace" title="Facebook" />
         </a>
         <a href="https://wa.me/5541996607553" target="_blank">
           <img src="" alt="" title="WhatsApp" />
