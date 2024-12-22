2f3cd05e3d631abb354fb48fa4603523d7d84183
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 17:43:03 -0300

    Alteramos o caminho dos icones

--- src/header.jsx
+++ src/header.jsx
@@ -1,6 +1,6 @@
-import LogoI from "/public/Icons/LogoInstag.ico";
-import LogoF from "/public/Icons/LogoFace.ico";
-import LogoW from "/public/Icons/LogoWhats.ico";
+//import LogoI from "/public/Icons/LogoInstag.ico";
+//import LogoF from "/public/Icons/LogoFace.ico";
+//import LogoW from "/public/Icons/LogoWhats.ico";
 
 const Header = () => {
   return (
@@ -12,15 +12,15 @@ const Header = () => {
       </nav>
       <div className="logos">
         <a href="https://www.instagram.com/goro_tattooo/" target="_blank">
-          {<img src={LogoI} alt="" title="" />}
+          {<img src="public/Icons/LogoInstag.ico" alt="" title="" />}
         </a>
 
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
           target="_blank">
-          {<img src={LogoF} alt="iconeFace" title="Facebook" />}
+          {<img src="public/Icons/LogoFace.ico" title="Facebook" />}
         </a>
         <a href="https://wa.me/5541996607553" target="_blank">
-          <img src={LogoW} alt="" title="WhatsApp" />
+          <img src="public/Icons/LogoWhats.ico" alt="" title="WhatsApp" />
         </a>
       </div>
     </header>
