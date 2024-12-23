02e7fe1f02441cea23bb4b0efd9846b0a6016f2a
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 21:52:47 -0300

    Alteramos uma pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Pages/Home";
 //import Galeria from "./Pages/Galeria";
 import EstilosTattoo from "/src/Pages/EstilosTattoo";
 import Curiosidades from "./Pages/Curiosidades";
-import Navegacao from "./Pages/Navegacao"
+//import Navegacao from "./Pages/Navegacao"
 //import Promo from "./Pages/Promo";
 //import Modelo from "./Pages/Modelo";
 
@@ -15,8 +15,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           {/*<Route exact path="Galeria" element={<Galeria />} />*/}
-          <Route exact path="/Pages/EstilosTattoo" element={<EstilosTattoo />} />
-          <Route exact path="/Pages/Navegacao" element={<Navegacao />} />
+          <Route exact path="///Pages/EstilosTattoo" element={<EstilosTattoo />} />
+          {/*<Route exact path="/Pages/Navegacao" element={<Navegacao />} />*/}
           <Route exact path="Pages/Curiosidades" element={<Curiosidades />} />
           {/* <Route exact path="Modelo" element={<Modelo />} />*/}
           {/* <Route exact path="Promo" element={<Promo />} />*/}
