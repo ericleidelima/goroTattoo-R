5298b0d2862abe8ff9113949f8bd7a6cec54efe9
USER 1 <user@USERs-Mac-mini.local>
2024-12-22 22:56:45 -0300

    Alteramos o endereço de curiosidades

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           
-          <Route exact path="/Pages/EstilosTattoo" element={<EstilosTattoo />} />
+          <Route exact path="Pages/EstilosTattoo" element={<EstilosTattoo />} />
           {/*<Route exact path="Galeria" element={<Galeria />} />*/}
           {/*<Route exact path="/Pages/Navegacao" element={<Navegacao />} />*/}
-          <Route exact path="/Pages/Curiosidades" element={<Curiosidades />} />
+          <Route exact path="Pages/Curiosidades" element={<Curiosidades />} />
           {/* <Route exact path="Modelo" element={<Modelo />} />*/}
           {/* <Route exact path="Promo" element={<Promo />} />*/}
         </Routes>
