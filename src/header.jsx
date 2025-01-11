f0475db956937189e66bdd2b7eb8ecfe212c91cc
USER 1 <user@USERs-Mac-mini.local>
2025-01-11 16:27:05 -0300

    Adicionamos o icone whatsapp

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,6 @@
 import LogoI from "/public/Icons/LogoInstag.jpg";
 import LogoF from "/public/Icons/LogoFace.jpg";
+import LogoW from "/public/icons/logoWhatsapp.png"
 
 
 const Header = () => {
@@ -18,6 +19,10 @@ const Header = () => {
         <a href="https://www.facebook.com/ericlei.lima.9/photos"
           target="_blank">
           {<img src={LogoF} title="Facebook" />}
+        </a>
+        <a href="https://api.whatsapp.com/send/?phone=5541996607553&text&type=phone_number&app_absent=0"
+          target="_blank">
+          {<img src={LogoW} title="whatsapp" />}
         </a>
         
       </div>
