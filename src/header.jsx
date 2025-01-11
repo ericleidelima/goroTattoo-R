85f29aedaa2fe95830812f9a8c447f4b0b4e143f
USER 1 <user@USERs-Mac-mini.local>
2025-01-11 16:28:43 -0300

    Adicionamos um icone

--- src/header.jsx
+++ src/header.jsx
@@ -1,6 +1,6 @@
 import LogoI from "/public/Icons/LogoInstag.jpg";
 import LogoF from "/public/Icons/LogoFace.jpg";
-import LogoW from "/public/icons/logoWhatsapp.png"
+import LogoW from "/public/icons/LogoFace.jpg"
 
 
 const Header = () => {
