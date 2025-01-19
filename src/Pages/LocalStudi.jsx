ccf4c50367ed55f696e22c9b892be6a01851a79c
USER 1 <user@USERs-Mac-mini.local>
2025-01-19 14:42:26 -0300

    Adicionamos um css a pagina localStudy

--- src/Pages/LocalStudi.jsx
+++ src/Pages/LocalStudi.jsx
@@ -27,47 +27,40 @@ const LocalStudi = () => {
 
           <img src={Local} alt="Local do estudio" />
           <h2>Regras do estudio</h2>
-          <p>
-            <ol>
-              <li>
-                Chegue preparado:
-                <br />
-                Chegue no horário combinado e, se possível, bem descansado e
-                alimentado.
-                <br />
-                Evite consumir álcool ou drogas antes da sessão.
-              </li>
-              <br />
-              <br />
-              <li>
-                Higiene pessoal:
-                <br />
-                Vá ao estúdio com a área da tatuagem limpa e sem produtos
-                químicos ou cremes.
-              </li>
-              <br />
-              <br />
-              <li>
-                Respeite o artista Seja claro sobresuas ideias, mas esteja
-                aberto a sugestões do tatuador. <br /> Evite levar muitas
-                pessoas ao estúdio ou criar distrações durante o trabalho.
-              </li>
-              <br />
-              <br />
 
-              <li>
-                Cuidados pós-tatuagem Siga rigorosamente as orientações do
-                artista sobre os cuidados após a tatuagem. <br /> Comunique
-                qualquer problema ou dúvida com o tatuador.
-              </li>
-              <br />
-              <br />
-              <li>
-                Seja honesto Informe sobre alergias, doenças ou condições de
-                saúde relevantes. <br />
-                Respeite os valores cobrados e pague pontualmente.
-              </li>
-            </ol>
+          <p>
+            Chegue preparado:
+            
+            Chegue no horário combinado e, se possível, bem descansado e
+            alimentado.
+            
+            Evite consumir álcool ou drogas antes da sessão.
+          </p>
+         
+          
+          <p>
+            Higiene pessoal:
+            
+            Vá ao estúdio com a área da tatuagem limpa e sem produtos químicos
+            ou cremes.
+          </p>
+         
+          <p>
+            Respeite o artista Seja claro sobresuas ideias, mas esteja aberto a
+            sugestões do tatuador. <br /> Evite levar muitas pessoas ao estúdio
+            ou criar distrações durante o trabalho.
+          </p>
+
+          <p>
+            Cuidados pós-tatuagem Siga rigorosamente as orientações do artista
+            sobre os cuidados após a tatuagem. <br /> Comunique qualquer
+            problema ou dúvida com o tatuador.
+          </p>
+          
+          <p>
+            Seja honesto Informe sobre alergias, doenças ou condições de saúde
+            relevantes. <br />
+            Respeite os valores cobrados e pague pontualmente.
           </p>
 
           <h2>Como é o nosso espaço</h2>
@@ -78,17 +71,16 @@ const LocalStudi = () => {
             <img src={Study3} alt="" />
             <img src={Study4} alt="" />
             <img src={Study5} alt="" />
-            
 
-            <video width="600px" controls>
+            <video id="video" controls autoPlay muted loop>
               <source src={video1Study} type="video/mp4" />
             </video>
           </div>
 
           <h3>sala de espera</h3>
           <p>
-          Sala de espera com Wi-Fi e telão com mais de 150 polegadas (aprox. 4 m), 
-          para o acompanhante ficar confortavelmente.
+            Sala de espera com Wi-Fi e telão com mais de 150 polegadas (aprox. 4
+            m), para o acompanhante ficar confortavelmente.
           </p>
           <div className="study">
             <img src={Study6} alt="" />
@@ -97,11 +89,10 @@ const LocalStudi = () => {
             <img src={Study9} alt="" />
             <img src={Study10} alt="" />
 
-            <video width="600px" controls>
+            <video id="video" controls autoPlay muted loop>
               <source src={video2Study} type="video/mp4" />
             </video>
           </div>
-          
         </article>
       </main>
       <Footer />
